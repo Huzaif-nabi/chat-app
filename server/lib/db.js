b95c29d5a035774552b79d57f86e57ed2a9f2582
Huzaif-nabi <huzaif.nabi22@spit.ac.in>
2025-08-06 03:10:51 +0530

    Backend Setup

---
+++ server/lib/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+// Function to connect to the mongodb database
+export const connectDB = async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`)
+        console.log("Connected to database");
+    } catch(error){
+        console.log(error);
+    }
+}
+
